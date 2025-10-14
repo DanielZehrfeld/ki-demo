b95dd1cf304990c45247ef99659e8f9119c676a6
COPRA\dz <daniel.zehrfeld@copra-system.de>
2025-10-14 17:47:55 +0200

    add backend signalr client

--- KiDemo/Backend/BackendService.cs
+++ KiDemo/Backend/BackendService.cs
@@ -1,4 +1,4 @@
-﻿//using KiChat.Client.SignalR;
+﻿using KiChat.Client.SignalR;
 using System;
 using System.Text;
 
@@ -7,50 +7,50 @@ namespace KiDemo.Backend;
 public static class BackendService
 {
 
-	//private static StringBuilder sb = new StringBuilder();
-	//
-	//private static SignalRClient? _client;
+	private static StringBuilder sb = new StringBuilder();
+	
+	private static SignalRClient? _client;
 
 
 
 	public static string ProcessUserInput(string input)
     {
-		//try
-		//{
-		//    if (_client == null)
-		//    {
-		//        sb.AppendLine("Client NULL, creating client");
-		//
-		//		var client = new SignalRClient();
-		//
-		//
-		//        client.RootResults.Subscribe(rr => sb.AppendLine($"{DateTimeOffset.Now:O} - RR: " + rr));
-		//        //10.0.0.5
-		//        //client.Run("http://localhost:5249", log => sb.AppendLine($"{DateTimeOffset.Now:O} - LOG: " + log));
-		//        client.Run("http://10.0.0.5:5249", log => sb.AppendLine($"{DateTimeOffset.Now:O} - LOG: " + log));
-		//
-		//		_client = client;
-		//    }
-		//
-		//
-		//    sb.AppendLine("releasing 1");
-		//
-		//	_client.CommandRelease(1);
-		//
-		//	sb.AppendLine("releasing DONE, sleeping");
-		//
-		//	Thread.Sleep(2000);
-		//
-		//	sb.AppendLine("sleeping DONE");
-		//}
-		//catch (Exception ex)
-		//{
-		//    sb.AppendLine($"{DateTimeOffset.Now:O} - ERROR: {ex}");
-		//}
-
-		//return $"processed: {sb}";
+		try
+		{
+		    if (_client == null)
+		    {
+		        sb.AppendLine("Client NULL, creating client");
 		
-		return $"processed x: {input}";
+				var client = new SignalRClient();
+		
+		
+		        client.RootResults.Subscribe(rr => sb.AppendLine($"{DateTimeOffset.Now:O} - RR: " + rr));
+		        //10.0.0.5
+		        //client.Run("http://localhost:5249", log => sb.AppendLine($"{DateTimeOffset.Now:O} - LOG: " + log));
+		        client.Run("http://10.0.0.5:5249", log => sb.AppendLine($"{DateTimeOffset.Now:O} - LOG: " + log));
+		
+				_client = client;
+		    }
+		
+		
+		    sb.AppendLine("releasing 1");
+		
+			_client.CommandRelease(1);
+		
+			sb.AppendLine("releasing DONE, sleeping");
+		
+			Thread.Sleep(2000);
+		
+			sb.AppendLine("sleeping DONE");
+		}
+		catch (Exception ex)
+		{
+		    sb.AppendLine($"{DateTimeOffset.Now:O} - ERROR: {ex}");
+		}
+
+		return $"processed: {sb}";
+		
+		//return $"processed x: {input}";
     }
 
 
